e5c181c8503cab9668d4a2c3a871496dbcfc8384
yejianfeng2014@126.com <yejianfeng2014@126.com>
2019-04-26 15:08:21 +0800

    添加统计，意图匹配的准确率

--- src/main/java/com/orderplus/UserNlp.java
+++ src/main/java/com/orderplus/UserNlp.java
@@ -3,42 +3,39 @@ package com.orderplus;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 
-import java.util.List;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 
 
-
 public class UserNlp {
 
 
+    public static void main(String[] args) {
+//
+       String  ss = "informed seller that item not received and they promised to refund. did not get any refund yet";
 
-        public static void main(String[] args)  {
-            String aString = "jhend925  https://blog.csdn.net/timo1160139211/article/details/77603141. All 2015 GTIs have heated seats, including the S trim level with no options. I used to own a 2013 335i with the news system and now own a 2011 M3 with the stalk system you talked about. The new system on the 2013 was much better. Ok, yeah, you have to turn it on every time, but it's much easier to adjust speed";
-            List<String> word = getlema(aString);
-            for (int i = 0; i < word.size(); i++) {
-                System.out.println(word.get(i));
-            }
-        }
+//        inf#####_#####med seller #####_##### item not received #####_##### they promised to refund. did not get any refund #####_#####
+
+        String[] sss = SplitSentenceByConj(ss);
+
+    }
 
     /**
      * 词形还原
+     *
      * @param string:字符串
      * @return List<String> 分词、提取词形还原后的结果
-     * */
-    public static List<String> getlema(String text){
+     */
+    public static List<String> getlema(String text) {
         //单词集合
-        List<String> wordslist = new ArrayList<>();;
+        List<String> wordslist = new ArrayList<>();
+        ;
         //StanfordCoreNLP词形还原
         Properties props = new Properties();  // set up pipeline properties
         props.put("annotators", "tokenize, ssplit, pos, lemma");   //分词、分句、词性标注和次元信息。
@@ -46,8 +43,8 @@ public class UserNlp {
         Annotation document = new Annotation(text);
         pipeline.annotate(document);
         List<CoreMap> words = document.get(CoreAnnotations.SentencesAnnotation.class);
-        for(CoreMap word_temp: words) {
-            for (CoreLabel token: word_temp.get(CoreAnnotations.TokensAnnotation.class)) {
+        for (CoreMap word_temp : words) {
+            for (CoreLabel token : word_temp.get(CoreAnnotations.TokensAnnotation.class)) {
                 String lema = token.get(CoreAnnotations.LemmaAnnotation.class);  // 获取对应上面word的词元信息，即我所需要的词形还原后的单词
                 wordslist.add(lema);
             }
@@ -56,31 +53,73 @@ public class UserNlp {
     }
 
 
-
-
-    public static String[] splitSentence(String cmt){
+    public static String[] splitSentence(String cmt) {
         /*正则表达式：句子结束符*/
-        String regEx=",|\\.|\\?|!|:|;|~|，|：|。|！|；|？";
-        Pattern p =Pattern.compile(regEx);
+        String regEx = ",|\\.|\\?|!|:|;|~|，|：|。|！|；|？";
+        Pattern p = Pattern.compile(regEx);
         Matcher m = p.matcher(cmt);
         /*按照句子结束符分割句子*/
         String[] words = p.split(cmt);
         /*将句子结束符连接到相应的句子后*/
-      /*  if(words.length > 0)
-        {
-            int count = 0;
-            while(count < words.length)
-            {
-                if(m.find())
-                {
-                    words[count] += m.group();
-                }
-                count++;
-            }
-        }*/
-        /*输出结果*/
+
         return words;
     }
+
+    /**
+     * 这个实在句子分割的基础后做的，所以里面没有标点符号，
+     *
+     * @param text
+     * @return
+     * 支持单个单词分割
+     */
+    public static String[] SplitSentenceByConj(String text) {
+        String splitLabels = "yet," +
+                "but," +
+                "so," +
+                "and," +
+                "or," +
+                "neithor," +
+                "nor," +
+                "when," +
+                "while," +
+                "as," +
+                "whenever," +
+                "before," +
+                "after," +
+                "since," +
+                "until," +
+                "till," +
+                "although," +
+                "though," +
+                "even though," +
+                "while," +
+                "however," +
+                "that";
+
+        String[] split = splitLabels.split(",");
+
+
+        String[] s1 = text.split(" ");
+
+        // 每一个单词核对
+        for (int i=0 ;i< s1.length;i++) {
+            for (String s2 : split) {
+                if (s1[i].trim().equals(s2)){
+                    s1[i] = "#####_#####";
+                }
+            }
+        }
+
+        String tempString = " ";
+        for (String s : s1) {
+            tempString = tempString + s +" " ;
+        }
+        String[] split1 = tempString.split("#####_#####");
+        return split1;
+
+
+    }
+
 
 
 
---
+++ src/main/scala/pypal_pre/intent/IntentMatch.scala
@@ -0,0 +1,126 @@
+package pypal_pre.intent
+
+import java.util.Properties
+
+import com.orderplus.UserNlp
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.functions.udf
+
+
+/**
+  *
+  * 查看当前的意图匹配能力水平，
+  * 1，直接匹配
+  * 2，句子匹配
+  * 3，短语匹配
+  *
+  *
+  * 测试数据量：45105
+  *
+  */
+object IntentMatch {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("buyer first dialogue")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val properties: Properties = new Properties()
+    properties.setProperty("user", "demo")
+    properties.setProperty("password", "123456")
+
+    val url = "jdbc:mysql://127.0.0.1:3306/local_first_2"
+    val url_2 = "jdbc:mysql://127.0.0.1:3306/local_test_1"
+
+    // 测试数据
+    val mysqlDF: DataFrame = spark.read.jdbc(url, "bt_paypal_dispute_info_pre_first_message_buyer", properties)
+
+    val mysqlDF_phrases: DataFrame = spark.read.jdbc(url_2, "bt_ai_intent_phrase", properties)
+
+
+
+    // 意图短语
+
+    val frame_intent_phrase: DataFrame = mysqlDF_phrases.select("intent_phrase")
+
+
+    val phrases = scala.collection.mutable.Set[String]()
+
+
+    frame_intent_phrase.collect().foreach { a =>
+      val str: String = a.getString(0)
+      phrases.+=(str)
+
+      Unit
+
+    }
+
+    print(phrases.size)
+
+
+    def getFirstBuyerString(str: String): Boolean = {
+
+      if (phrases.contains(str)) true else false
+
+    }
+
+
+    val my_udf = udf(getFirstBuyerString _) //将自定义函数注册为udf
+
+    val out = mysqlDF.withColumn("level_1", my_udf($"message_2")) //使用udf进行转换操作
+
+
+    // 采用分句子的模式
+
+
+    def getFirstBuyerString_2(str: String): Boolean = {
+
+      val strings = UserNlp.splitSentence(str)
+
+      val booleans = strings.map(a => phrases.contains(a))
+      if (booleans.contains(true)) true else false
+
+
+    }
+
+
+    val my_udf_2 = udf(getFirstBuyerString_2 _) //将自定义函数注册为udf
+
+
+    val out_2 = out.withColumn("level_2", my_udf_2($"message_2")) //使用udf进行转换操作
+
+    // 连接词级别：
+
+    def getFirstBuyerString_3(str: String): Boolean = {
+
+      val strings = UserNlp.splitSentence(str)
+
+      val booleans = strings.flatMap(a => UserNlp.SplitSentenceByConj(a)).map(a => phrases.contains(a.trim))
+
+      if (booleans.contains(true)) true else false
+
+    }
+
+
+    val my_udf_3 = udf(getFirstBuyerString_3 _) //将自定义函数注册为udf
+
+
+    val out_3 = out_2.withColumn("level_3", my_udf_3($"message_2")) //使用udf进行转换操作
+
+    out_3.write.mode(saveMode = SaveMode.Overwrite).jdbc(url, "bt_paypal_dispute_info_pre_first_message_buyer_leve1", properties)
+
+    
+    // 添加上连接词的分割，达到了7% 左右
+
+    spark.stop()
+
+  }
+
+
+}
